73585bf22e9a977d837e0ef9d3f2d6f421cb7fb4
kgovardhan409 <kgovardhan409@gmail.com>
2026-04-03 19:33:12 +0530

    subnet corrected

--- dev/main.tf
+++ dev/main.tf
@@ -8,13 +8,13 @@ data "aws_ami" "amazon_linux" {
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 }
-resource "aws_instance" "my_ec2_dev_instance" {
+resource "aws_instance" "dev_instance" {
   instance_type = "t3.micro"
   ami           = data.aws_ami.amazon_linux.id
-  subnet_id     = "subnet-044ba859d55c33e82"
+  subnet_id     = "subnet-0aaeda47ba0e1165a"
   user_data     = file("userdata.sh")
   tags = {
-    Name = "my_ec2_dev_instance"
+    Name = "dev_instance"
   }
 
 
