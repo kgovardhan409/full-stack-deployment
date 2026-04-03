0a0536e743c1447f96297911633d5c464ce1edf2
kgovardhan409 <kgovardhan409@gmail.com>
2026-04-03 19:45:02 +0530

    user data updated

--- dev/userdata.sh
+++ dev/userdata.sh
@@ -1,9 +1,5 @@
-yum update -y
-
-yum install -y httpd
-
-systemctl start httpd
-
-systemctl enable httpd
-
-echo "<h1>Welcome from Terraform EC2 🚀</h1>" > /var/www/html/index.html
+#!/bin/bash
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo echo "<h1>fullsatack-dev-app</h1>" > /var/www/html/index.html
